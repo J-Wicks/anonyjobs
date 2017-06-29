8dbe1aab64460d5a64bbaace65ca7f3447f4f674
David R Lowry Lowry <ross@Davids-MacBook-Pro.local>
2017-06-29 18:15:23 -0400

    added skills model, udpated seed file with skills;  create  profile page and individual profile pages still in process; minor updates from previous commit

--- browser/reducers/skillsReducer.js
+++ browser/reducers/skillsReducer.js
@@ -6,10 +6,7 @@ import axios from 'axios';
 /* ----------------------- CONSTANTS -----------*/
 // All applications for all users
 const SET_ALL_SKILLS = 'SET_ALL_SKILLS';
-// All applications for one user
-// const SET_USER_APPLICATIONS = 'SET_USER_APPLICATIONS';
-// One single application for one user;
-const SET_USER_APPLICATION = 'SET_USER_APPLICATION';
+
 
 const initialState = {
 	allSkills: []
@@ -19,21 +16,15 @@ const initialState = {
 
 /*---- action cretors */
 
-// awaiting model completion
+// in process...
 
 /*------------------ REDUCER */
 
 export default function (state = initialState, action) {
   const newState = Object.assign({}, state);
   switch (action.type) {
-    case SET_ALL_APPLICATIONS:
-      newState.allApplications = action.applications;
-      break;
-    case SET_USER_APPLICATIONS:
-      newState.userApplications = action.applications;
-      break;
-    case SET_USER_APPLICATION:
-      newState.userApplication = action.application;
+    case SET_ALL_SKILLS:
+      newState.allSkills = action.skills;
       break;
     default:
       return state;
