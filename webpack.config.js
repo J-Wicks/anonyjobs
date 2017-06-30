b5086d18550a2b94cfb7b16aa3d9ece3dd690337
Jeremy Wicks <artfullu@gmail.com>
2017-06-30 16:20:44 -0400

    Added es2015 to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         options: {
-          presets: ['react'] // if you aren't using 'babel-preset-es2015', then omit the 'es2015'
+          presets: ['react', 'es2015'] // if you aren't using 'babel-preset-es2015', then omit the 'es2015'
         }
       },
       {
