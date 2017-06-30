ed59b52aaa6ce2e16802b6dfa8f637b1dd5bc1d0
Jeremy Wicks <artfullu@gmail.com>
2017-06-30 17:09:22 -0400

    added some code

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,25 @@ module.exports = {
   },
   devtool: 'source-map',
   module: {
+    { test: /\.js?$/,
+        loader: 'babel',
+        exclude: path.join(__dirname, 'node_modules') },
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         options: {
-          presets: ['react', 'es2015'] // if you aren't using 'babel-preset-es2015', then omit the 'es2015'
+          presets: ['react'] // if you aren't using 'babel-preset-es2015', then omit the 'es2015'
         }
+      },
+      {
+            test: /\.scss$/,
+            use: [
+              'style-loader',
+              'css-loader',
+              'sass-loader'
+            ]
       }
     ]
   }
